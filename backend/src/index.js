9399ef7c3302f0e0c292b094c6b5a79e954cb901
Rohin1212 <rohinchopra1212@gmail.com>
2021-07-14 22:30:11 +1000

    REFACTOR : simplified the handler

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,4 @@
-exports.index = async (event, context) => {
-  let response
-
-  try {
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Welcome to DigiShelf API'
-      })
-    }
-  } catch (err) {
-    console.log(err)
-    return err
-  }
-
-  return response
-}
+exports.index = async (event, context) => ({
+  statusCode: 200,
+  body: 'Welcome to DigiShelf API'
+})
