dbc33fd269d286e0fc5a1dc192b75f1afed1e846
Rohin1212 <rohinchopra1212@gmail.com>
2021-07-26 18:41:53 +1000

    ADD: created a button component

---
+++ frontend/src/components/Button/ButtonElements.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  display: inline-block;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  padding: 6px 8px;
+  border: 1px solid transparent;
+  padding: 1rem;
+  border-radius: 10px;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  font-size: 1rem;
+  padding: 0.5rem 0.75rem;
+  cursor: pointer;
+`
