1f32ea66849cbbaae989ded6fb429bae447f69e6
Rohin1212 <rohinchopra1212@gmail.com>
2021-07-30 13:26:30 +1000

    REFACTOR : moved to tailwindcss

--- frontend/src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Button as StyledButton } from './ButtonElements'
-
-const Button = ({ className, children }) => {
-  return <StyledButton className={className}>{children}</StyledButton>
-}
-
-export default Button
