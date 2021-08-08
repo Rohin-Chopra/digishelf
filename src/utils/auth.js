38528a8f3383a9090cc0e999ee03858476866c58
Rohin1212 <rohinchopra1212@gmail.com>
2021-08-08 16:44:20 +1000

    ADD: added auth utils

---
+++ src/utils/auth.js
@@ -0,0 +1,47 @@
+import { Auth } from 'aws-amplify'
+
+export const signUp = async (username, password, { email, name }) => {
+  try {
+    const { user } = await Auth.signUp({
+      username,
+      password,
+      attributes: {
+        email,
+        name
+      }
+    })
+    return user
+  } catch (error) {
+    return [null, error]
+  }
+}
+
+export const verifyUser = async (username, code) => {
+  try {
+    await Auth.confirmSignUp(username, code)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+export const signIn = async (username, password) => {
+  try {
+    const user = await Auth.signIn(username, password)
+    return user
+  } catch (error) {
+    return [null, error]
+  }
+}
+
+export const getCurrentUser = async () => {
+  const user = await Auth.currentAuthenticatedUser()
+  if (user === null) {
+    return null
+  }
+  const token = user.signInUserSession.idToken.jwtToken
+  return {
+    user,
+    token
+  }
+}
