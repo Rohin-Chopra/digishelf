ce07dd085eeec087d4f35119ab8114d46d2e480c
Rohin1212 <rohinchopra1212@gmail.com>
2021-08-13 14:03:12 +1000

    ADD : added add shelf media and get shelf pages

--- src/pages/GetShelf.js
+++ src/pages/GetShelf.js
@@ -1,16 +1,28 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { API } from 'aws-amplify'
-import { getAccessJwtToken, getCurrentUser } from '../utils/auth'
+import { getCurrentUser } from '../utils/auth'
 import Button from '../components/Button'
+import movieDb from '../api/movieDb'
+import { FaPlusCircle } from 'react-icons/fa'
 
-const Media = () => {
+const Media = ({ media }) => {
+  const [imgUrl, setImgUrl] = useState('')
+  const fetchMedia = async () => {
+    const { data } = await movieDb.get(`/${media.type}/${media.id}`, {
+      params: { api_key: '4e02f274e1c605a2e0b08bac93f177e4' }
+    })
+    setImgUrl(data.poster_path)
+  }
+  useEffect(() => {
+    fetchMedia()
+  }, [])
+
   return (
     <div>
       <img
         className='max-h-96 rounded shadow-lg mt-2'
         alt='random'
-        src='https://image.tmdb.org/t/p/w500//qAZ0pzat24kLdO3o8ejmbLxyOac.jpg'
+        src={`https://image.tmdb.org/t/p/w500//${imgUrl}`}
       />
     </div>
   )
@@ -22,8 +34,9 @@ const GetShelf = ({ history, match }) => {
 
   const fetchShelf = async () => {
     const { user, token } = await getCurrentUser()
-    const { data } = await axios.get(
-      `https://sdhr3phfz1.execute-api.ap-southeast-2.amazonaws.com/dev/shelves/${match.params.id}`,
+    const { shelf } = await API.get(
+      'digishelfAPI',
+      `/shelves/${match.params.id}`,
       {
         headers: {
           Authorization: token
@@ -31,7 +44,7 @@ const GetShelf = ({ history, match }) => {
       }
     )
     setUserName(user.attributes.name)
-    setShelf(data.shelf)
+    setShelf(shelf)
   }
 
   useEffect(() => {
@@ -39,25 +52,34 @@ const GetShelf = ({ history, match }) => {
   }, [])
 
   return (
-    <div className='container px-6 py-4'>
-      <h1 className='prose text-xl font-bold'>
-        {userName}'s {shelf?.name}
-      </h1>
-      <p className='prose font-light'>{shelf?.description}</p>
-      <Button
-        className='bg-green-500 text-white ml-auto'
-        onClick={() => history.push(`/shelves/${match.params.id}/media/add`)}
-      >
-        Add Media
-      </Button>
-      <div className='mt-2 grid md:grid-cols-4 justify-items-center'>
-        {/* {
-         shelf.Media.map(()=>{
-
-         })
-       } */}
+    <main className='flex flex-col'>
+      <div className='container px-6 py-4 mx-auto'>
+        <div className='flex justify-between'>
+          <div>
+            <h1 className='prose text-xl font-bold'>
+              {userName}'s {shelf?.name}
+            </h1>
+            <p className='prose font-light'>{shelf?.description}</p>
+          </div>
+          <div>
+            <Button
+              className='bg-green-500 text-white ml-auto'
+              onClick={() =>
+                history.push(`/shelves/${match.params.id}/media/add`)
+              }
+            >
+              Add Media
+              <FaPlusCircle className='ml-1 inline text-xl' />
+            </Button>
+          </div>
+        </div>
+        <div className='mt-2 grid md:grid-cols-4 gap-4 justify-items-center'>
+          {shelf?.Media.map((media) => {
+            return <Media key={media.id} media={media} />
+          })}
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
