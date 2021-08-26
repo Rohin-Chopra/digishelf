b871e5a0ee940ffdf2d48eecd42222b1ce9d41e9
Rohin1212 <rohinchopra1212@gmail.com>
2021-08-26 23:43:26 +1000

    added users name

--- src/components/DiscoverCard.js
+++ src/components/DiscoverCard.js
@@ -6,7 +6,7 @@ const DiscoverCard = ({ shelf, history }) => {
     <div className='shadow-lg rounded-lg w-80 my-4 relative cursor-pointer w-80 md:w-96' onClick={() => history.push(`/${shelf.id}`)}>
       <div className='py-2 px-2'>
         <Gravatar email={shelf.createdBy} className='rounded-full inline' size={35} />
-        <span className='ml-2 prose prose-sm text-gray-600'>Rohin Chopra</span>
+        <span className='ml-2 prose prose-sm text-gray-600'>{shelf.createdByName}</span>
       </div>
       <div>
         <img src={`https://rohin-bucket.s3.ap-southeast-2.amazonaws.com/${shelf.coverImg}`}
