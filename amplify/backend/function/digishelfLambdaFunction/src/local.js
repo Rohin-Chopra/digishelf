d6c0ef0b8c5f2e4e2b63ee648654b7b9dff80b54
Rohin1212 <rohinchopra1212@gmail.com>
2022-10-03 23:20:01 +1100

    nextjs app init

--- amplify/backend/function/digishelfLambdaFunction/src/local.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: '.prod.env' })
-const app = require('./app')
-
-const port = 5000
-
-app.listen(port, () => {
-  console.log(`Digishelf API listening on port ${port}`)
-})
